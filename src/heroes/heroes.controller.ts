f7003dfe032e67626ded8822701706d34e94564d
Ruslan Gonzalez <ruslanguns@gmail.com>
2022-02-26 22:38:54 +0000

    Adding log to controller

--- src/heroes/heroes.controller.ts
+++ src/heroes/heroes.controller.ts
@@ -4,21 +4,25 @@ import { KillDragonCommand } from './commands/impl/kill-dragon.command';
 import { KillDragonDto } from './interfaces/kill-dragon-dto.interface';
 import { Hero } from './models/hero.model';
 import { GetHeroesQuery } from './queries/impl';
+import { StoreAndLogService } from './store-and-log.service';
 
 @Controller('hero')
 export class HeroesGameController {
   constructor(
     private readonly commandBus: CommandBus,
     private readonly queryBus: QueryBus,
+    private readonly logger: StoreAndLogService,
   ) {}
 
   @Post(':id/kill')
   async killDragon(@Param('id') id: string, @Body() dto: KillDragonDto) {
+    this.logger.log(`Hero ${id} is killing dragon`);
     return this.commandBus.execute(new KillDragonCommand(id, dto.dragonId));
   }
 
   @Get()
   async findAll(): Promise<Hero[]> {
+    this.logger.log(`Hero is getting all heroes`);
     return this.queryBus.execute(new GetHeroesQuery());
   }
 }
